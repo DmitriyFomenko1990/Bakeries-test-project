0a5bf83b0d7e17c3235cdbb914d30969529f577b
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-08-23 16:50:52 +0300

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,152 @@
+<template>
+  <footer class="footer__wrapper">
+    <div class="footer__content">
+      <div class="footer__settings">
+        <div class="footer__social">
+          <button class="footer__first-social-logo footer__social-btn"></button>
+          <button class="footer__second-social-logo footer__social-btn"></button>
+        </div>
+        <h4 class="footer__settings-title">Countries</h4>
+        <div class="footer__countries">
+          <button class="footer__first-country footer__flag"></button>
+          <button class="footer__second-country footer__flag"></button>
+        </div>
+        <h4 class="footer__settings-title">Language</h4>
+        <div class="footer__languages">
+          <button>NO</button>
+          <p>|</p>
+          <button>EN</button>
+        </div>
+      </div>
+      <div class="footer__nav">
+        <h3 class="footer__nav-title">Customer service</h3>
+        <ul class="footer__list">
+          <li class="footer__list-item">FAQ</li>
+          <li class="footer__list-item">Terms of use</li>
+          <li class="footer__list-item">Privacy policy</li>
+          <li class="footer__list-item">Contact us</li>
+        </ul>
+      </div>
+      <div class="footer__nav">
+        <h3 class="footer__nav-title">Information</h3>
+        <ul class="footer__list">
+          <li class="footer__list-item">How it works</li>
+          <li class="footer__list-item">About Cake it easy</li>
+          <li class="footer__list-item">For companies</li>
+          <li class="footer__list-item">Send cakes to multiple locations</li>
+          <li class="footer__list-item">Cake calendar</li>
+        </ul>
+      </div>
+      <div class="footer__nav">
+        <h3 class="footer__nav-title">Inspiration</h3>
+        <ul class="footer__list">
+          <li class="footer__list-item">Birthday cake</li>
+          <li class="footer__list-item">Cake with image or logo</li>
+          <li class="footer__list-item">Confirmation cake</li>
+          <li class="footer__list-item">Baptism cake</li>
+          <li class="footer__list-item">Glutenfree cake</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .footer {
+    &__wrapper{
+      background-color: #f3f3f3;
+      padding: 20px 100px;
+    }
+    &__content{
+      display: flex;
+      justify-content: space-between;
+    }
+    &__settings{
+      width: 70px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    &__social{
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 15px;
+      padding: 0 6px;
+    }
+    &__social-btn{
+      position: relative;
+      width: 25px;
+      height: 25px;
+      border-radius: 50%;
+      border: 1px solid #000;
+      &:after {
+        content: "";
+        position: absolute;
+        width: 16px;
+        height: 16px;
+        left: 4px;
+        top: 4px;
+      }
+    }
+    &__first-social-logo{
+      &:after {
+        background: url("../assets/social/first-social1.svg")  center no-repeat;
+      }
+    }
+    &__second-social-logo{
+      &:after {
+        background: url("../assets/social/second-social2.svg")  center no-repeat;
+      }
+    }
+    &__settings-title{
+      margin-bottom: 8px;
+      font-size: 12px;
+    }
+    &__countries{
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 15px;
+    }
+    &__flag{
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+    }
+    &__first-country{
+      background:  url("../assets/flags/first-flag.svg")  center no-repeat;
+    }
+    &__second-country{
+      background:  url("../assets/flags/second-flag.svg")  center no-repeat;
+    }
+    &__languages{
+      display: flex;
+      justify-content: space-between;
+      padding: 0 5px;
+    }
+    &__nav{
+      text-align: left;
+    }
+    &__nav-title{
+      font-size: 16px;
+      font-weight: 600;
+      margin-bottom: 12px;
+    }
+    &__list-item{
+      margin-bottom: 10px;
+      font-weight: 400;
+      font-size: 14px;
+      color: #a6a6a6;
+      &:last-child{
+        margin-bottom: 0;
+      }
+    }
+  }
+</style>
