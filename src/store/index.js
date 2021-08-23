c0247f6d4eb7e465da20a311a61353aa755847ca
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-08-23 23:29:38 +0300

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { fetchBakeries } from '../api/api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isError: '',
+    language: 'EN',
+    bakeries: []
   },
   mutations: {
+    setError (state, error) {
+      state.isError = error
+    },
+    setLanguage (state, language) {
+      state.language = language
+    },
+    setBakeries (state, bakeries) {
+      state.bakeries = bakeries
+    }
   },
   actions: {
+    fetchBakeries ({ context }) {
+      return fetchBakeries()
+        .then(response => {
+          context.commit('setBakeries', response)
+        })
+        .catch(error => {
+          context.commit('setError', error)
+        })
+    }
   },
   modules: {
   }
